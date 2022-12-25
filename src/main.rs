44b991d8513b9fdf77bd3c2ec2b3b79f78c296b7
naonao708 <naonao708@outlook.com>
2022-12-25 07:40:22 +0000

    Add loop func

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::io;
 
 fn main() {
-    println!("Which temperature unit Celsius or Fahrenheit? (C/F)");
-    let mut select = String::new();
-    io::stdin()
-        .read_line(&mut select)
-        .expect("Failed to read line");
+    loop {
+        println!("Which temperature unit Celsius or Fahrenheit? (C/F)");
+        let mut select: String = String::new();
+        io::stdin()
+            .read_line(&mut select)
+            .expect("Failed to read line");
 
-    let temp: char = match select.trim().parse() {
-        Ok(char) => char,
-        Err(_) => continue,
-    };
-    
+        match select.as_str() {
+            "C" => println!("C to F"),
+            "F" => println!("F to C"),
+            &_ => println!("&str"),
+        }
+    }
 }
