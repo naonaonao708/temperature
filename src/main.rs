2680281a1d5268d5203fdac68a160bfb2d8699a4
naonao708 <naonao708@outlook.com>
2022-12-27 07:54:00 +0000

    Change script

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 use std::io;
 
 fn main() {
-    loop {
-        println!("Which temperature unit Celsius or Fahrenheit? (C/F)");
-        let mut select: String = String::new();
+    'outer: loop {
+        println!("To change from Celsius to Fahrenheit, press 1.");
+        println!("To change from Fahrenheit to Celsius, press 2.");
+
+        // Get input and convert to integer
+        let mut command = String::new();
         io::stdin()
-            .read_line(&mut select)
+            .read_line(&mut command)
             .expect("Failed to read line");
 
-        match select.as_str() {
-            "C" => println!("C to F"),
-            "F" => println!("F to C"),
-            &_ => println!("&str"),
+        let command: u32 = match command.trim().parse() {
+            Ok(num) => {
+                // If input is invalid, let users try again
+                if num != 1 && num != 2 {
+                    continue;
+                }
+                num
+            },
+            Err(_) => continue,
+        };
+
+        loop {
+            println!("Input the temperature.");
+
+            // Get input and convert to float
+            let mut temperature = String::new();
+            io::stdin()
+                .read_line(&mut temperature)
+                .expect("Failed to read line");
+
+            let temperature: f32 = match temperature.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+            // Calculate converted temperature
+            if command == 1 {
+                println!("{} C = {} F", temperature, temperature*1.8 + 32.0);
+            } else {
+                println!("{} F = {} C", temperature, (temperature - 32.0)/1.8);
+            }
+
+            break 'outer;
         }
     }
 }
